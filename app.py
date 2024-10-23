ab6bde58a9746405d6b8010e83e34ebb40e8957d
Hoangg Anh <131897682+LucciHank@users.noreply.github.com>
2024-10-23 16:53:08 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -60,5 +60,5 @@ def predict():
     result = predict_image(image)
     return jsonify(result)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
